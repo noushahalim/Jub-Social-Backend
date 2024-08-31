96253038317a89b82fa9140408fda17f7c853944
noushahalim <noushahalim@gmail.com>
2024-08-31 08:11:01 +0530

    signUp resend otp completed.

--- controllers/authController.js
+++ controllers/authController.js
@@ -132,3 +132,39 @@ exports.signupOtpVerification = async(req,res)=>{
         res.status(500).json('Internal server error');
     }
 }
+
+exports.signupResendOtp = async(req,res)=>{
+    try{
+        const id=req.body.id
+        const client = await signupModel.findOne({_id:id})
+        if(!client){
+            return res.status(400).json('user details not getting')
+        }
+        else{
+            const emailOtp = generateOtp()
+            const sentMail = await emailContent.generateEmailContent(emailOtp);
+            
+            const mailOptions = {
+                from:process.env.NODE_MAILER_GMAIL,
+                to:client.email,
+                subject:'Welcome to Jub Social! Verify Your Account',
+                html: sentMail
+            }
+
+            nodemailer.sentMailOtp(mailOptions)
+
+            await signupModel.findOneAndUpdate(
+                {_id:id},
+                {
+                    otp:emailOtp
+                }
+            )
+            res.status(200).json('otp resented')
+            
+        }
+    }
+    catch(err){
+        console.log('error on signupResendOtp',err);
+        res.status(500).json('Internal server error');
+    }
+}
--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController')
 
 clientRouter.post('/signup',authController.clientSignup)
 clientRouter.post('/signupOtpVerification',authController.signupOtpVerification)
+clientRouter.post('/signupResendOtp',authController.signupResendOtp)
 
 module.exports = clientRouter
